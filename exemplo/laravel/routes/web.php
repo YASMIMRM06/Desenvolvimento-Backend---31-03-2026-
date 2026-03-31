1fbb9608d7c7b0341210f642b659b57fa6ec41ac
yasmim russi mariano <yasmimrussi@gmail.com>
2026-03-31 11:22:40 -0300

    arrumando e tentando me achar

--- exemplo/laravel/routes/web.php
+++ exemplo/laravel/routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/enviar-email', function () {
+   dd('Email enviado!'); 
+});
+
+   #iremos enviar o email aqui e depois iremos criar uma fila
